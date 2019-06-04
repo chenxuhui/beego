b009cc80ebd8b2c5e62cb551b0ca1d88e1aa3de0
chenxuhui <chenxuhui@gmail.com>
2019-06-04 13:27:16 +0800

    Fix delete old function

--- logs/file.go
+++ logs/file.go
@@ -338,15 +338,15 @@ func (w *fileLogWriter) deleteOldLog() {
 		}
         if w.Hourly {
             if !info.IsDir() && info.ModTime().Add(1 * time.Hour * time.Duration(w.MaxHours)).Before(time.Now()) {
-                if strings.HasPrefix(filepath.Base(path), filepath.Base(w.fileNameOnly)) &&
-                strings.HasSuffix(filepath.Base(path), w.suffix) {
+                if strings.HasPrefix(filepath.Base(path), filepath.Base(w.fileNameOnly) + w.suffix) &&
+                !strings.HasSuffix(filepath.Base(path), w.suffix) {
                     os.Remove(path)
                 }
             }
         } else if w.Daily {
             if !info.IsDir() && info.ModTime().Add(24 * time.Hour * time.Duration(w.MaxDays)).Before(time.Now()) {
-                if strings.HasPrefix(filepath.Base(path), filepath.Base(w.fileNameOnly)) &&
-                strings.HasSuffix(filepath.Base(path), w.suffix) {
+                if strings.HasPrefix(filepath.Base(path), filepath.Base(w.fileNameOnly) + w.suffix) &&
+                !strings.HasSuffix(filepath.Base(path), w.suffix) {
                     os.Remove(path)
                 }
             }
